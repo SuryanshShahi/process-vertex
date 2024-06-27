c093a65672c899fda72e677d478809397d009621
unknown <designkrunchstudio@gmail.com>
2024-06-27 08:15:19 +0530

    seo

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,6 +1,6 @@
 export const metaContent = {
-  TITLE: "",
-  DESCRIPTION: "",
-  SITENAME: "",
+  TITLE: "AIXGrow: Elevating Brands for the Future Elite",
+  DESCRIPTION: "Explore AIXGrow's visionary strategies tailored for elite clientele. Revolutionize your brand's presence with advanced, personalized solutions.",
+  SITENAME: "AIXGrow: Elevating Brands for the Future Elite",
   FAVICON: "favicon/favicon.ico",
 };
